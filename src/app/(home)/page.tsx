060e6bffa4b6dc4a836c5fba67cb69ec9bdc17c6
OHS <ohs6006@gmail.com>
2024-02-18 18:21:11 +0900

    Test : Git Action Deploy 테스트

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,6 +4,7 @@ const HomePage: React.FunctionComponent<IHomePageProps> = () => {
   return (
     <div className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div>Home Page</div>
+      <div>Deploy Test!!</div>
     </div>
   );
 };
