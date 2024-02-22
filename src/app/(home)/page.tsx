eab0c4f10f10313ecc3bfefefb566f3861105b1a
camac0808 <zqzwzazs@gmail.com>
2024-02-22 19:15:48 +0900

    resolve conflict

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,23 +6,7 @@ const HomePage: React.FunctionComponent<IHomePageProps> = () => {
   return (
     <div className='w-full flex flex-col items-center justify-between'>
       <Notification content={"메이플래닛 베타 서비스 중 입니다"}/>
-      <div className='p-20'>
-        <div>Home Page</div>
-        <div>Deploy Test!!</div>
-      </div>
-      <div className='p-20'>
-        <div>Home Page</div>
-        <div>Deploy Test!!</div>
-      </div><div className='p-20'>
-        <div>Home Page</div>
-        <div>Deploy Test!!</div>
-      </div><div className='p-20'>
-        <div>Home Page</div>
-        <div>Deploy Test!!</div>
-      </div><div className='p-20'>
-        <div>Home Page</div>
-        <div>Deploy Test!!</div>
-      </div><div className='p-20'>
+      <div className='p-20 mt-[120px] text-white'>
         <div>Home Page</div>
         <div>Deploy Test!!</div>
       </div>
