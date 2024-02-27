e095d10397339b6fee926438c664e8f15ceecbb9
OHS <ohs6006@gmail.com>
2024-02-28 01:10:17 +0900

    Fix : notification bug 수정

--- src/components/ui/notification.tsx
+++ src/components/ui/notification.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Icon from './icon';
-import Inner from './inner';
 
 interface INotificationProps {
   content: string;
@@ -9,16 +8,14 @@ interface INotificationProps {
 const Notification: React.FunctionComponent<INotificationProps> = ({ content }) => {
   return (
     <div className='fixed top-[60px] z-50 h-[40px] w-full overflow-hidden bg-[#CE98B1]'>
-      <Inner>
-        <div className='flex h-[40px] animate-slide-in-left items-center justify-center space-x-4'>
-          <Icon src='/svgs/snail.svg' alt='snail' size={26} />
-          <div className='text-lg font-bold text-[#7C0747]'>
-            <span className='mr-5'>[공지사항]</span>
-            <span>{content}</span>
-          </div>
-          <Icon src='/svgs/snail.svg' alt='snail' size={26} />
+      <div className='flex h-[40px] animate-slide-in-left items-center justify-center space-x-4'>
+        <Icon src='/svgs/snail.svg' alt='snail' size={26} />
+        <div className='text-lg font-bold text-[#7C0747]'>
+          <span className='mr-5'>[공지사항]</span>
+          <span>{content}</span>
         </div>
-      </Inner>
+        <Icon src='/svgs/snail.svg' alt='snail' size={26} />
+      </div>
     </div>
   );
 };
