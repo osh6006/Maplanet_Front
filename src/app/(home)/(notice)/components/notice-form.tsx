ed4483a4b173a950e847b2edb8db48b20b796442
OHS <ohs6006@gmail.com>
2024-03-01 20:56:49 +0900

    Feat : notice-write page validation

--- src/app/(home)/(notice)/components/notice-form.tsx
+++ src/app/(home)/(notice)/components/notice-form.tsx
@@ -18,18 +18,32 @@ const NoticeForm: React.FunctionComponent<INoticeFormProps> = (props) => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className='mx-auto space-y-4 lg:w-1/2'>
-      <NoticeInput name='title' label='제목' control={control} placeholder='제목을 입력해 주세요' />
+      <NoticeInput
+        name='title'
+        label='제목'
+        control={control}
+        placeholder='제목을 입력해 주세요'
+        rules={{
+          required: '제목을 입력해 주세요'
+        }}
+      />
       <NoticeInput
         name='category'
         label='분류'
         control={control}
         placeholder='분류를 입력해 주세요'
+        rules={{
+          required: '분류를 입력해 주세요'
+        }}
       />
       <NoticeTextArea
         label='내용'
         name='content'
         control={control}
         placeholder='내용을 입력해 주세요'
+        rules={{
+          required: '내용을 입력해 주세요'
+        }}
       />
       <Button size='wide' color='main'>
         등록
