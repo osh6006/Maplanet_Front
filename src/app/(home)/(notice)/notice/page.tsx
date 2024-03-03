9e0b690c6faa2c1b5327a2ad42f2d04f99ca3ded
OHS <ohs6006@gmail.com>
2024-03-04 08:27:56 +0900

    Fix : Build error Fix

--- src/app/(home)/(notice)/notice/page.tsx
+++ src/app/(home)/(notice)/notice/page.tsx
@@ -1,10 +1,10 @@
+import { Suspense } from 'react';
+import Link from 'next/link';
 import Inner from '@/components/ui/inner';
 import Button from '@/components/ui/button';
-import NoticeList from '../components/notice-list';
-import NoticeHeader from '../components/notice-header';
-import NoticePageNation from '../components/notice-pagination';
-import Link from 'next/link';
 import Banner from '@/components/ui/banner';
+import NoticeList from '../components/notice-list';
+import NoticePageNation from '../components/notice-pagination';
 
 interface INoticePageProps {}
 
@@ -14,19 +14,21 @@ const NoticePage: React.FunctionComponent<INoticePageProps> = ({}) => {
   return (
     <main className='space-y-5'>
       <Banner title='공지사항' />
-      <Inner>
-        <div className='mb-4 flex w-full justify-end'>
-          <Link href={'/notice-write'}>
-            <Button color='main' size='sm'>
-              등록하기
-            </Button>
-          </Link>
-        </div>
-        <NoticeList />
-        <div className='flex w-full items-center justify-center'>
-          <NoticePageNation itemsPerPage={5} totalPost={123} />
-        </div>
-      </Inner>
+      <Suspense>
+        <Inner>
+          <div className='mb-4 flex w-full justify-end'>
+            <Link href={'/notice-write'}>
+              <Button color='main' size='sm'>
+                등록하기
+              </Button>
+            </Link>
+          </div>
+          <NoticeList />
+          <div className='flex w-full items-center justify-center'>
+            <NoticePageNation itemsPerPage={5} totalPost={123} />
+          </div>
+        </Inner>
+      </Suspense>
     </main>
   );
 };
