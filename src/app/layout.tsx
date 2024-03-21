cdc9f4d10f65daf517afde2a61aaa46e44e7acf7
OHS <ohs6006@gmail.com>
2024-03-21 17:20:30 +0900

    Chore : 네이버 메타 태그 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Noto_Sans_KR } from 'next/font/google';
 import RootProvider from '../providers/root-provider';
 
 import './globals.css';
+import Head from 'next/head';
 
 const notoSansKR = Noto_Sans_KR({
   subsets: ['latin']
@@ -34,6 +35,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ko'>
+      <Head>
+        <meta name='naver-site-verification' content='a51ae85576bbf5ec28c7d8dd3511a8e624c97cc2' />
+      </Head>
       <body
         className={`${notoSansKR.className} h-[100dvh] overflow-y-auto overflow-x-hidden bg-background text-white`}>
         <RootProvider>{children}</RootProvider>
