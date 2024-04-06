08c3fd29e808c84f63211a0bfe6c430c5e573c63
OHS <ohs6006@gmail.com>
2024-04-06 23:00:54 +0900

    Fix : home 버튼 z-index 수정

--- src/app/(home)/components/edge-buttons.tsx
+++ src/app/(home)/components/edge-buttons.tsx
@@ -12,10 +12,10 @@ const DISCORD_HREF = process.env.NEXT_PUBLIC_DISCORD_CHANNEL_URL;
 
 const EdgeButtons: React.FunctionComponent<IDiscordChannelBtnProps> = ({ accessToken }) => {
   return (
-    <div className='fixed bottom-8 right-4 flex flex-col items-end gap-y-4 sm:right-10'>
+    <div className='fixed bottom-8 right-4 z-50 flex flex-col items-end gap-y-4 sm:right-10'>
       <div className='sm:hidden'>
         <button
-          className=' relative z-20 flex items-center justify-center gap-x-2 rounded-full bg-main px-4 py-2 transition-all active:scale-95'
+          className=' relative flex items-center justify-center gap-x-2 rounded-full bg-main px-4 py-2 transition-all active:scale-95'
           onClick={() => {
             if (!accessToken) toast.error('로그인을 해주세요!');
           }}>
